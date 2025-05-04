ccfd26730431980af0b5a829cb8836e6b58988b0
Sindhuja573 <sindhuja2k16@gmail.com>
2025-05-04 22:10:56 +0530

    personal information

--- src/app/components/personal/personal.component.ts
+++ src/app/components/personal/personal.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { UserServiceService } from '../user-service.service';
 import { Router } from '@angular/router';
 
@@ -10,115 +10,81 @@ import { Router } from '@angular/router';
 })
 export class PersonalComponent {
 
-  userForm!:FormGroup;
+  userForm = this.fb.group({
+    fname: ['', Validators.required],
+    mname: ['', Validators.required],
+    lname: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    gender: ['', Validators.required],
+    bloodGroup: ['', Validators.required],
+    dob: ['', Validators.required],
+    merital: ['', Validators.required],
+    marriage_date: [''], // optional
 
-  fname = '';
-  mname = '';
-  lname = '';
-  email = '';
-  gender = '';
-  bloodGroup = '';
-  dob = '';
-  merital = '';
-  marriegedate = '';
-  current_address = '';
-  current_country = '';
-  current_state = '';
-  current_city = '';
-  current_pincode = '';
-  current_contact = '';
-  permanent_address = '';
-  permanent_country = '';
-  permanent_state = '';
-  permanent_city = '';
-  permanent_pincode = '';
-  permanent_contact = '';
-  bcp_address = '';
-  bcp_country = '';
-  bcp_state = '';
-  bcp_city = '';
-  bcp_pincode = '';
-  emergency_contact_name = '';
-  emergency_contact_number = '';
-  emergency_relationship = '';
-  year = '';
-  month = '';
-  relevantYear = '';
+    current_address: ['', Validators.required],
+    current_country: ['', Validators.required],
+    current_state: ['', Validators.required],
+    current_city: ['', Validators.required],
+    current_pincode: ['', [Validators.required, Validators.pattern(/^\d{6}$/)]],
+    current_contact: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+
+    permanent_address: ['', Validators.required],
+    permanent_country: ['', Validators.required],
+    permanent_state: ['', Validators.required],
+    permanent_city: ['', Validators.required],
+    permanent_pincode: ['', [Validators.required, Validators.pattern(/^\d{6}$/)]],
+    permanent_contact: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+
+    emergency_contact: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+    emergency_name: ['', Validators.required],
+    emergency_relationship: ['', Validators.required],
+  });
+
   constructor(
-    private formBuilder:FormBuilder,
-    private userService:UserServiceService,
-    private router:Router,
-  ){
-    this.userForm = this.formBuilder.group({
-      fname:['',Validators.required],
-      mname:[''],
-      lname:['',Validators.required],
-      email:['',[Validators.required,
-        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]
-      ],
-      gender:['',Validators.required],
-      bloodGroup:['',Validators.required],
-      dob:['',Validators.required],
-      merital:['',Validators.required],
-      marriegedate:[''],
-      current_address:['',Validators.required],
-      current_country:['',Validators.required],
-      current_state:['',Validators.required],
-      current_city:['',Validators.required],
+    private fb: FormBuilder,
+    private userService: UserServiceService,
+    private router: Router
+  ) {}
 
-      current_pincode: ['', [Validators.required, Validators.pattern(/^\d{6}$/)]],
-
-
-      current_contact:['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
-      permanent_country:['',Validators.required],
-      permanent_address:['',Validators.required],
-      permanent_state:['',Validators.required],
-      permanent_city:['',Validators.required],
-
-      permanent_pincode:['', [Validators.required, Validators.pattern(/^\d{6}$/)]],
-
-      permanent_contact:['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
-      bcp_address:['',Validators.required],
-      bcp_country:['',Validators.required],
-      bcp_state:['',Validators.required],
-      bcp_city:['',Validators.required],
-
-      bcp_pincode:['', [Validators.required, Validators.pattern(/^\d{6}$/)]],
-
-      emergency_contact_name:['',Validators.required],
-
-      emergency_contact_number:['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
-      emergency_relationship:['',Validators.required],
-      year:['',Validators.required],
-      month:['',Validators.required],
-      relevantYear:['',Validators.required],
-    })
-  };
-
-  // submitForm() {
-  //   if (this.userForm.valid) {
-  //     console.log('Form Submitted', this.userForm.value);
-  //     this.userService.setFormData('personal', this.userForm.value);
-  //     this.router.navigate(['/kyc']);
-  //   } else {
-  //     alert("All fields are mandatory");
-  //     console.log("This form is invalid!");
-  //   }
-  // }
-  
   submitForm() {
-    if (this.userForm.valid) {
-      console.log('Form Submitted', this.userForm.value);
-      this.userService.setFormData('Personal Data', this.userForm.value);
-      // console.log('Stored Data:', this.userService.getFormData());
-      this.userForm.reset();
-      this.router.navigate(['/kyc']);
+    if (this.userForm.invalid) {
+      this.userForm.markAllAsTouched();
+      alert("Please fill all required fields correctly.");
+      return;
+    }
+
+    console.log('Form Submitted', this.userForm.value);
+    this.userService.setFormData('Personal Data', this.userForm.value);
+    this.userForm.reset();
+    this.router.navigate(['/kyc']);
+  }
+
+  onSameAsCurrentChange(event: any): void {
+    const checked = event.target.checked;
+
+    if (checked) {
+      this.userForm.patchValue({
+        permanent_address: this.userForm.value.current_address,
+        permanent_country: this.userForm.value.current_country,
+        permanent_state: this.userForm.value.current_state,
+        permanent_city: this.userForm.value.current_city,
+        permanent_pincode: this.userForm.value.current_pincode,
+        permanent_contact: this.userForm.value.current_contact
+      });
     } else {
-      alert("All fields are mandatory");
-      console.log("This form is invalid!");
+      this.userForm.patchValue({
+        permanent_address: '',
+        permanent_country: '',
+        permanent_state: '',
+        permanent_city: '',
+        permanent_pincode: '',
+        permanent_contact: ''
+      });
     }
   }
-  
-  
-  
+
+  // For template: helper method to access controls easily
+  get f() {
+    return this.userForm.controls;
+  }
 }
